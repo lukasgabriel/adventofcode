453cfe3a0a358e846115c20ed75ed54af6e75a4a
Lukas Gabriel <lukasgabriel@pm.me>
2023-03-04 18:27:58 +0100

    Start 2015 Day 8

---
+++ 2015/8/2015_08_01.py
@@ -0,0 +1,14 @@
+# Advent of Code 2015, Day 8: Matchsticks. Part 1
+# http://adventofcode.com/day/8
+
+
+def main():
+    solution = 0
+    for line in open("./2015/7/2015_08.txt", "r", encoding="utf-8"):
+        pass
+
+    return f"The resulting number of characters is {solution}"
+
+
+if __name__ == "__main__":
+    print(main())
