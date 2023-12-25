7aa226f83dc94ff082c4fdd8b02b0db2570ee511
Lukas Gabriel <lukasgabriel@pm.me>
2023-12-26 00:35:33 +0100

    Add 2023 Day 1

---
+++ 2023/1/2023_01_01.py
@@ -0,0 +1,19 @@
+# Advent of Code 2015, Day 1: Trebuchet?! Part 1
+# https://adventofcode.com/2013/day/1
+
+
+import re
+
+
+def main():
+    solution = 0
+    
+    for line in open("./2023/1/2023_01.txt", "r", encoding="utf-8"):
+        digits = re.findall(r"\d", line)
+        solution += int("".join(digits[0] + digits[-1]))
+       
+    return f"The sum of all values is {solution}"
+
+
+if __name__ == "__main__":
+    print(main())
